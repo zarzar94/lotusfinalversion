816f1dd621df990643cb204f1b911ce02efc54f7
zarzar94 <zarzar.abdulrahman@gmail.com>
2025-12-16 07:09:32 +0400

    deploy: publish dist

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Header } from './Header';
-// SlideViewer, Checklist, GameSection, ContactForm are lazy-loaded in App.tsx
-export { styles, brandPurple, brandCyan, brandPink } from './styles';
